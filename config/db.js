9e1a82d5565535e57b1c84c030be60275639afa6
YASHWANTH2601 <yashwanth2601@gmail.com>
2024-10-23 11:07:27 +0530

    db changed

--- config/db.js
+++ config/db.js
@@ -1,21 +1,3 @@
-// module.exports = connectDB;
-
-// const mongoose = require("mongoose");
-
-// const connectDB = async () => {
-//   return (
-//     mongoose
-//       .connect(
-//         "mongodb+srv://yashwanth2601:4DlS6TSCFHENwJcn@contact.dfi5h.mongodb.net/?retryWrites=true&w=majority&appName=contact"
-//       )
-//       // .connect(process.env.MONGODB_URI)
-//       .then(() => console.log(`connection to database established...`))
-//       .catch((err) => console.log(err))
-//   );
-// };
-
-// module.exports = connectDB;
-
 const mongoose = require("mongoose");
 require("dotenv").config(); // Load environment variables
 
